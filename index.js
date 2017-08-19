56e5676e285ccf205c62c3cff367776d8a4c7580
ramihaviv <30841431+ramihaviv@users.noreply.github.com>
2017-08-19 10:04:28 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+onDeviceReady: function() {
+  app.receivedEvent('deviceready');
+},
+  // Update DOM on a Received Event
+receivedEvent: function(id) {
+  var parentElement = document.getElementById(id);
+  var listeningElement = parentElement.querySelector('.listening');
+  var receivedElement = parentElement.querySelector('.received');
+
+  listeningElement.setAttribute('style', 'display:none;');
+  receivedElement.setAttribute('style', 'display:block;');
+
+  console.log('Received Event: ' + id);
+}
