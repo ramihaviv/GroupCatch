d4cc746d6923292851aca87f0215934db1c63124
ramihaviv <30841431+ramihaviv@users.noreply.github.com>
2017-08-19 10:19:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ receivedEvent: function(id) {
   var parentElement = document.getElementById(id);
   var listeningElement = parentElement.querySelector('.listening');
   var receivedElement = parentElement.querySelector('.received');
+  
 
   listeningElement.setAttribute('style', 'display:none;');
   receivedElement.setAttribute('style', 'display:block;');
